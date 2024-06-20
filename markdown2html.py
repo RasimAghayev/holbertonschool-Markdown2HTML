7693889c0fde8c82f98cc5e0c9d3a0a91f35a677
Rasim Aghayev <rasimaqayev@gmail.com>
2024-06-21 02:08:24 +0400

    0. Start a script 1.2

--- markdown2html.py
+++ markdown2html.py
@@ -6,12 +6,16 @@
 
 import sys
 import os.path
+import re
+import hashlib
 
 if __name__ == '__main__':
     if len(sys.argv) < 3:
-        print('Usage: ./markdown2html.py README.md README.html', file=sys.stderr)
+        print('Usage: ./markdown2html.py README.md README.html',
+              file=sys.stderr)
         exit(1)
 
     if not os.path.isfile(sys.argv[1]):
         print('Missing {}'.format(sys.argv[1]), file=sys.stderr)
         exit(1)
+    exit (0)
